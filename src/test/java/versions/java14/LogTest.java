b021ab9837352495927b6d2fc7f3e18754f8e03a
petershowx <xiaoyulong1988@gmail.com>
2020-06-19 20:06:40 +0800

    pom.xml src\test\java\versions\java14\LogTest.java

--- src/test/java/versions/java14/LogTest.java
+++ src/test/java/versions/java14/LogTest.java
@@ -1,9 +1,12 @@
 /**
- * 
+ *
  */
 package versions.java14;
 
-import org.junit.jupiter.api.Test;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.junit.Test;
 
 /**
  * @author xiaoy
@@ -13,30 +16,28 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 class LogTest {
-	@Test
-	void show() {
-		log.debug("Hello, {}.", "Debug");
-		log.info("Hello, {}.", "Info");
-		log.error("Hello, {}.", "Error");
-	}
-	
-	static Map<Integer, Integer> cache = new ConcurrentHashMap<>();
+    static Map<Integer, Integer> cache = new ConcurrentHashMap<>();
 
+    static int fibonacci(int i) {
+        if (i == 0)
+            return i;
+        if (i == 1)
+            return 1;
+        return cache.computeIfAbsent(i, (key) -> {
+            System.out.println("Slow calculation of " + key);
+            return fibonacci(i - 2) + fibonacci(i - 1);
+        });
+    }
 
-	    public static void main(String[] args) {
-	        System.out.println("f(" + 14 + ") =" + fibonacci(14));
-	    }
+    public static void main(String[] args) {
+        System.out.println("f(" + 14 + ") =" + fibonacci(14));
+    }
 
-	    static int fibonacci(int i) {
-	        if (i == 0)
-	            return i;
-	        if (i == 1)
-	            return 1;
-	        return cache.computeIfAbsent(i, (key) -&gt; {
-	            System.out.println("Slow calculation of " + key);
-	            return fibonacci(i - 2) + fibonacci(i - 1);
-	        });
-	    }
-
+    @Test
+    void show() {
+        log.debug("Hello, {}.", "Debug");
+        log.info("Hello, {}.", "Info");
+        log.error("Hello, {}.", "Error");
+    }
 
 }
